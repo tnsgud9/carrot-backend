27e1dbb3a71c85cdde328914f1e1e59254634c62
tnsgud9 <tnsgud9@naver.com>
2022-01-04 21:43:52 +0900

    SQL init, example school database control

---
+++ src/controller/school.controller.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import SchoolModel from "../model/school.model";
+
+type School = {
+  name: string;
+};
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const schools: SchoolModel[] = await SchoolModel.findAll();
+  return res.status(200).json(schools);
+});
+
+router.get("/:schoolId", async (req, res) => {
+  const { schoolId } = req.params;
+  if (!schoolId) {
+    return res.status(400).json();
+  }
+  const schoolIdNumber: number = parseInt(schoolId, 10);
+  const school: SchoolModel | null = await SchoolModel.findByPk(schoolIdNumber);
+  if (!school) {
+    res.status(404).json();
+  }
+  return res.status(200).json(school);
+});
+
+router.post("/", async (req, res) => {
+  const school = req.body as School;
+  if (!school) {
+    return res.status(400).json();
+  }
+  SchoolModel.create({
+    name: school.name,
+  });
+  return res.status(201).json();
+});
+
+export default router;
